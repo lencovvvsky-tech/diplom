3bf08f439ef47ee1a866edceaa32d20572d0cfb2
Lencovvvsky <lencovvvsky@gmail.com>
2026-03-19 00:42:10 +0300

    Update db.js to support Railway env vars
    Commit changes

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
+    user: process.env.PGUSER || process.env.DB_USER,
+    password: process.env.PGPASSWORD || process.env.DB_PASSWORD,
+    host: process.env.PGHOST || process.env.DB_HOST,
+    port: process.env.PGPORT || process.env.DB_PORT || 5432,
+    database: process.env.PGDATABASE || process.env.DB_DATABASE,
 });
 
 module.exports = pool;
