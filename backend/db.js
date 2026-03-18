abd193bfcdee60cbd159badead2b7bcfbeb9d7cc
Lencovvvsky <lencovvvsky@gmail.com>
2026-03-19 01:02:47 +0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,12 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.PGUSER || process.env.DB_USER,
-    password: process.env.PGPASSWORD || process.env.DB_PASSWORD,
-    host: process.env.PGHOST || process.env.DB_HOST,
-    port: process.env.PGPORT || process.env.DB_PORT || 5432,
-    database: process.env.PGDATABASE || process.env.DB_DATABASE,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+    connectionTimeoutMillis: 10000,
 });
 
 module.exports = pool;
